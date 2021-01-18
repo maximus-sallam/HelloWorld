a583f33d744c0b22aba8af7600706bb80fb795b6
Maximus Sallam <maximus.sallam@gmail.com>
2021-01-18 18:57:42 +0800

    small edits

--- Program.cs
+++ Program.cs
@@ -10,19 +10,12 @@ namespace HelloWorld
             int[] source = { 12, 1, 5, -2, 16, 14 }; // Source numbers
             int[] dest = { 1, 2, 3, 4, 5 }; // Numbers I will call from
             int[] userAge = { 21, 22, 23, 24, 25 }; // Declaration of age
-            int[] numbers = { 12, 1, 5, -2, 16, 14 }; 
-
-            Console.WriteLine("Hello, grandma! You're " + userAge[4] + " years old!"); // You be old. Get that checked out.
-            
+            int[] numbers = { 12, 1, 5, -2, 16, 14 };
+            Console.WriteLine("Hello, grandma! You're " + userAge[4] + " years old!"); // You be old. Get that checked out
             Array.Copy(source, dest, 3); // Copying first 3 values from 'source' into 'dest'
             Array.Sort(numbers); // Sort list of 'numbers'
-
-            /* Commenting during testing -----
-            Console.WriteLine(dest[3]); // Calling 4th value from 'dest'
-            ----- Commenting during testing */
-            
+            Console.WriteLine(dest[3]); // Calling 4th value from 'dest'            
             Console.WriteLine(numbers[0]); // Calling 1st value from sorted 'numbers'
-
             Console.Read();
         }
     }
