39c166b44436f979ff9bb2fe52c1a9a3225c236e
Maximus Sallam <maximus.sallam@gmail.com>
2021-01-18 18:26:39 +0800

    added age declaration.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] userAge = { 21, 22, 23, 24, 25 }; // Declaration
+            Console.WriteLine("Hello, grandma! You're " + userAge[4] + " years old!");
             Console.Read();
         }
     }
