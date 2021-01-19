54d7132144881a19e98741cb7cf7483379c4f712
Maximus Sallam <maximus.sallam@gmail.com>
2021-01-19 18:41:39 +0800

    Made program interactive using WriteLine

--- Program.cs
+++ Program.cs
@@ -10,31 +10,38 @@ namespace HelloWorld
         {
             int[] source = { 12, 1, 5, -2, 16, 14 }; // Source array
             int[] dest = { 1, 2, 3, 4, 5 }; // Array I will call from
-            int[] userAge = { 21, 22, 23, 24, 25 }; // Declaration of age / array of ages 
-            int[] numbers = { 12, 1, 5, -2, 16, 14 };
+            int[] userAge = { 21, 22, 23, 24, 25 }; // Array of ages 
+            int[] numbers = { 12, 1, 5, -2, 16, 14 }; // Array of random numbers
 
-            string message = "Hello, Grandma. You're ";
-            string anotherMessage = " years old!";
-            string consoleMessage = message + userAge[4] + anotherMessage; // Consolidate parts of message 
+            string message = "Hello, Grandma. You're {0} years old!";
             string doYou = "Do you recognize any of these names?";
 
             string[] separator = { ", ", "; " }; // Adding string separators
             string names = "Rick Ross, Ricky Bobby; Rick James, , Rick Sanchez"; // Let's get Rick Rolled - Adding string of Rick's
             string[] substrings = names.Split(separator,
-                StringSplitOptions.RemoveEmptyEntries); // Splitting string of Ricks into array of substrings
+                StringSplitOptions.RemoveEmptyEntries); // Splitting string of Rick's into substring array
 
             List<int> userAgeList = new List<int> { 11, 21, 31, 41 }; // List of numbers
 
-            Console.WriteLine(consoleMessage); // You be old. Get that checked out
+            string question = "Choose a number from 1 to 5: ";
+            Console.WriteLine(question); // Ask for a number, stored as string
+            
+            string num = Console.ReadLine(); // Stores number entry to 'num' string
+            int newNum = Convert.ToInt32(num); // Converts string to int
+            newNum--; // Reduces the value of newNum by one
+            Console.WriteLine(message, userAge[newNum]); // You be old. Get that checked out - Prints newNum to console
             
             Array.Copy(source, dest, 3); // Copying first 3 values from 'source' array into 'dest' array
             Array.Sort(numbers); // Sort 'numbers' array
 
-            Console.WriteLine(dest[3]); // Calling 4th value (4) from 'dest'            
-            Console.WriteLine(numbers[0]); // Calling 1st value (-2) from sorted 'numbers'
-            Console.WriteLine(userAgeList[0]); // Calling 1st value (11) from List
+            //Console.WriteLine(dest[3]); // Calling 4th value (4) from 'dest'            
+            //Console.WriteLine(numbers[0]); // Calling 1st value (-2) from sorted 'numbers'
+            //Console.WriteLine(userAgeList[0]); // Calling 1st value (11) from List
+            
             Console.WriteLine(doYou); // Print 'doYou' string
-            Console.WriteLine(substrings[0] + "\n" + substrings[3]); // Calling substrings from split string of Rick's
+            
+            //Console.WriteLine(substrings[0] + "\n" + substrings[3]); // Calling substrings from split string of Rick's
+            
             foreach (var item in substrings) // Print substring array to console
             {
                 Console.WriteLine(item.ToString());
